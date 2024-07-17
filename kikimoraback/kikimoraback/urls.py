7ae54fb943609430f84e1fe7d023ace989c2b5cd
Sweet_Sempai <chcolatemilk00@gmail.com>
2024-07-17 23:07:27 +0300

    views and urls
    create ferst views for admin panel

--- kikimoraback/kikimoraback/urls.py
+++ kikimoraback/kikimoraback/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from django.views.generic import RedirectView
+# TODO поправить редирект, чтобы небыло поддомена
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('shop.urls')),
 ]
