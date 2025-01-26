4856c66e83c405d60c9e31cbe68608feb0c954a7
Sweet_Sempai <chcolatemilk00@gmail.com>
2025-01-26 18:06:22 +0300

    Значительно переработал item_check_builder - Добавил подфункцию для формирования словаря с товаром - Добавил обработку краевых случаев - Добавил комментариев к коду

--- kikimoraback/shop/API/yookassa_api.py
+++ kikimoraback/shop/API/yookassa_api.py
@@ -13,7 +13,7 @@ logger.setLevel(logging.DEBUG)
 key = 'test__kXJEEjyiSkZNQzUGm5nb5EwNtgjz4HHAIBXojqhYMU'
 id = '1007767'
 
-# TODO: Доработать использование баллов, после ввода Decimal стали появляться ошибки, возможно стоит добавить проверку, можно ли списать все баллы с стоимости одного товара
+
 class PaymentYookassa:
     def __init__(self):
         Configuration.configure(os.getenv("YOOMONEY_ID_2"), os.getenv("YOOMONEY_KEY_2"))
@@ -35,47 +35,60 @@ class PaymentYookassa:
                 list: Список товаров и услуги доставки в формате для оплаты.
             """
         bonuses = Decimal(bonuses or 0)
-        remaining_bonuses = bonuses
-        last_product = cart['products'][-1]
         items = []
 
         cart_total = Decimal(cart['total'])
 
-        for product in cart['products']:
-            product_price = Decimal(product['price'])
+        def create_item(product:dict, bonus, quantity=None)->dict:
+            """Создаем словари товара для вставки в счет на оплату"""
+            item = {
+            "description": product['name'],
+            "quantity": quantity if quantity else product['quantity'],
+            "amount": {
+                "value": float((Decimal(product['price']) - bonus).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)),
+                "currency": "RUB"
+            },
+            "vat_code": "1",
+            "payment_mode": "prepayment",
+            "payment_subject": "commodity",
+            "country_of_origin_code": "RU",
+            "measure": "piece"}
+            return item
+        # Находим товар с самой большой стоимостью,
+        # чтобы потом попытаться погасить все балы из его цены,
+        # вместо распределения баллов по всему чеку
+        max_price_product = max(cart['products'], key=lambda x: x['price'])
 
-            if product != last_product:
-                bonus_for_product = (product_price / cart_total * bonuses).quantize(
-                    Decimal('0.01'), rounding=ROUND_HALF_UP)
-                remaining_bonuses -= bonus_for_product
-                items.append({
-                    "description": product['name'],
-                    "quantity": product['quantity'],
-                    "amount": {
-                        "value": float((product_price - bonus_for_product).quantize(Decimal('0'), rounding=ROUND_HALF_UP)),
-                        "currency": "RUB"
-                    },
-                    "vat_code": "1",
-                    "payment_mode": "prepayment",
-                    "payment_subject": "commodity",
-                    "country_of_origin_code": "RU",
-                    "measure": "piece"
-                })
-            else:
-                items.append({
-                    "description": product['name'],
-                    "quantity": product['quantity'],
-                    "amount": {
-                        "value": float((product_price - remaining_bonuses).quantize(Decimal('0'), rounding=ROUND_HALF_UP)),
-                        "currency": "RUB"
-                    },
-                    "vat_code": "1",
-                    "payment_mode": "prepayment",
-                    "payment_subject": "commodity",
-                    "country_of_origin_code": "RU",
-                    "measure": "piece"
-                })
+        if max_price_product['price'] - int(bonuses)>=1 and bonuses > 0:
+            # Списываем бонусы с этого товара
+            items.append(create_item(max_price_product, bonuses, 1))
+            # Если колличество товара с которого можно за раз списать все баллы больше 1,
+            # То мы первый товар добавляем в чек с измененой ценой,
+            # а оставшиеся его экземпляры добавляем как обычные товары
+            if max_price_product['quantity']>1:
+                items.append(create_item(max_price_product, 0, max_price_product['quantity']-1))
+            # Добавляем остальные товары без учета бонусов
+            for product in cart['products']:
+                if product != max_price_product:
+                    items.append(create_item(product, 0))
+        else:
+            # Распределяем бонусы по всем товарам
+            remaining_bonuses = bonuses
+            last_product = cart['products'][-1]
+            for product in cart['products']:
+                product_price = Decimal(product['price'])
+                if product != last_product:
+                    bonus_for_product = (product_price / cart_total * bonuses).quantize(
+                        Decimal('0.01'), rounding=ROUND_HALF_UP)
+                    remaining_bonuses -= bonus_for_product
+                    items.append(create_item(product, bonus_for_product))
+                else:
+                    # Для последнего товара распределяем бонусы равномерно,
+                    # чтобы избежать ошибки когда при большом числе товаров
+                    # цена товара становиться отрицательной
+                    items.append(create_item(product, remaining_bonuses/product['quantity']))
 
+        # Добавляем доставку
         delivery_cost = delivery_data['deliveryCost']
         items.append({
             "description": delivery_data['deliveryMethod'],
@@ -88,12 +101,10 @@ class PaymentYookassa:
             "payment_mode": "prepayment",
             "payment_subject": "service",
         })
-
         return items
 
     def send_payment_request(self, user_data, cart, order_id, delivery_data, bonuses):
         bonuses = bonuses or 0
-        print(self.item_check_builder(cart, delivery_data, bonuses))
         payement = \
             Payment.create({
                 "amount": {
