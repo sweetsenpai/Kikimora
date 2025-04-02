579316634fd52495607032086d97a3c32100b627
Aleksander <chcolatemilk00@gmail.com>
2025-04-02 16:31:56 +0300

    Update entrypoint.sh

--- kikimoraback/entrypoint.sh
+++ kikimoraback/entrypoint.sh
@@ -4,9 +4,11 @@
 python manage.py makemigrations
 python manage.py migrate
 
-# Собираем статик в /var/www/kikimora/static/
+# Запускаем задачу для инициализации кэша
+python manage.py boot_cache
+
+# Собираем статические файлы
 python manage.py collectstatic --noinput
 
 # Запускаем gunicorn
 exec gunicorn --bind 0.0.0.0:8000 --capture-output kikimoraback.wsgi:application
-
