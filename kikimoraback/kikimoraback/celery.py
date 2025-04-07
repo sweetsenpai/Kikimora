66111061edc835cd00afef593fcf873cdcc6106a
Sweet_Sempai <chcolatemilk00@gmail.com>
2025-04-07 19:09:51 +0300

    Переписал зависимости от старого settings.py на новые настрои

--- kikimoraback/kikimoraback/celery.py
+++ kikimoraback/kikimoraback/celery.py
@@ -5,7 +5,7 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kikimoraback.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kikimoraback.settings_dev")
 
 app = Celery("kikimoraback")
 
